f83b5122a348d61d37e4a4c01a8b0ed606b77769
octavio.martinez <octavio.martinez@oneadvanced.com>
2021-01-27 11:12:39 +0000

    priority list component, invoke

---
+++ ViewComponents/PriorityListViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewComponentSample.Models;
+
+namespace StarterViewComp.ViewComponents
+{
+    public class PriorityListViewComponent : ViewComponent
+    {
+        private readonly ToDoContext db;
+
+        public PriorityListViewComponent(ToDoContext context)
+        {
+            db = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+        int maxPriority, bool isDone)
+        {
+            List<TodoItem> items = await GetItemsAsync(maxPriority, isDone);
+            return View(items);
+        }
+        private Task<List<TodoItem>> GetItemsAsync(int maxPriority, bool isDone)
+        {
+            return db.ToDo.Where(x => x.IsDone == isDone &&
+                                 x.Priority <= maxPriority).ToListAsync();
+        }
+    }
+}
